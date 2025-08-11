19ea6bf6e359447edd9521facaf2ae1d81dfc27f
lubailong <lubailong@iie.ac.cn>
2025-08-11 22:43:37 +0800

    干净的分支上进行异常优先级改动
    修改bmc和大页巨页异常的优先级顺序
    
    读bitmap时，进行pmp/pma检查
    
    由于要区分更细致的异常类型，向下传递了一些更具体的读写类型参数

--- src/isa/riscv64/system/mmu.c
+++ src/isa/riscv64/system/mmu.c
@@ -52,7 +52,6 @@ typedef union PageTableEntry {
 #ifdef CONFIG_RV_MBMC
 #define BMBASE(bma) (bma << BMSHFT)
 #define GET_BIT(bm_base, ppn) (((bm_base[(ppn) / 8] >> ((ppn) % 8)) & 1))
-static int pt_level = 0;
 #endif
 
 // Sv39 & Sv48 page walk
@@ -268,9 +267,6 @@ vaddr_t get_effective_address(vaddr_t vaddr, int type) {
 paddr_t gpa_stage(paddr_t gpaddr, vaddr_t vaddr, int type, int trap_type, bool ishlvx, bool is_support_vs){
   Logtr("gpa_stage gpaddr: " FMT_PADDR ", vaddr: " FMT_WORD ", type: %d", gpaddr, vaddr, type);
   int max_level = 0;
-  #ifdef CONFIG_RV_MBMC
-  pt_level = 0;
-  #endif
   if (hgatp->mode == HGATP_MODE_BARE) {
     return gpaddr;
   } else if (hgatp->mode == HGATP_MODE_Sv48x4){
@@ -332,6 +328,26 @@ paddr_t gpa_stage(paddr_t gpaddr, vaddr_t vaddr, int type, int trap_type, bool i
       break;
     } else if (ISNDEF(CONFIG_RV_SVNAPOT) && pte.n) {
       break;
+    } else if (level > 0){
+      // superpage
+      word_t pg_mask = ((1ull << VPNiSHFT(level)) - 1);
+      if ((pg_base & pg_mask) != 0) {
+        // misaligned superpage
+        break;
+      } else if (pte.n) {
+        // superpage but napot
+        break;
+      }
+      pg_base = (pg_base & ~pg_mask) | (gpaddr & pg_mask & ~PGMASK);
+    } else if(pte.n) {
+      if ((pte.ppn & SVNAPOTMASK) != 0b1000) {
+          break;
+      }
+      word_t pg_mask = ((1ull << SVNAPOTSHFT) - 1);
+      pg_base = (pg_base & ~pg_mask) | (gpaddr & pg_mask & ~PGMASK);
+     } else if (! (MUXDEF(CONFIG_RV_MBMC, check_paddr_mbmc(pg_base, type == MEM_TYPE_IFETCH ? MEM_TYPE_IFETCH_READ : type == MEM_TYPE_WRITE ? MEM_TYPE_WRITE_READ : MEM_TYPE_READ, trap_type, MODE_S, vaddr), true))){
+        // should never go into this branch.
+        break;
     } else if (!pte.u) {
       break;
     } else if (
@@ -344,24 +360,6 @@ paddr_t gpa_stage(paddr_t gpaddr, vaddr_t vaddr, int type, int trap_type, bool i
       // TODO: support hardware a/d update.
       break;
     } else {
-      if (level > 0) {
-        // superpage
-        word_t pg_mask = ((1ull << VPNiSHFT(level)) - 1);
-        if ((pg_base & pg_mask) != 0) {
-          // misaligned superpage
-          break;
-        } else if (pte.n) {
-          // superpage but napot
-          break;
-        }
-        pg_base = (pg_base & ~pg_mask) | (gpaddr & pg_mask & ~PGMASK);
-      } else if (pte.n) {
-        if ((pte.ppn & SVNAPOTMASK) != 0b1000) {
-          break;
-        }
-        word_t pg_mask = ((1ull << SVNAPOTSHFT) - 1);
-        pg_base = (pg_base & ~pg_mask) | (gpaddr & pg_mask & ~PGMASK);
-      }
       cpu.pbmt = pte.pbmt;
       return pg_base | (gpaddr & PAGE_MASK);
     }
@@ -483,14 +481,7 @@ static paddr_t ptw(vaddr_t vaddr, int type) {
       if (level < 0) { goto bad; }
     }
   }
-#ifdef CONFIG_RVH
-  if (!check_permission(&pte, true, vaddr, type, virt, mode)) return MEM_RET_FAIL;
-#else
-  if (!check_permission(&pte, true, vaddr, type)) return MEM_RET_FAIL;
-#endif
-  #ifdef CONFIG_RV_MBMC
-  pt_level = level;
-  #endif
+
   if (level > 0) {
     // superpage
     word_t pg_mask = ((1ull << VPNiSHFT(level)) - 1);
@@ -509,6 +500,17 @@ static paddr_t ptw(vaddr_t vaddr, int type) {
     word_t pg_mask = ((1ull << SVNAPOTSHFT) - 1);
     pg_base = (pg_base & ~pg_mask) | (vaddr & pg_mask & ~PGMASK);
   }
+
+#ifdef CONFIG_RV_MBMC
+  if (!virt){
+      check_paddr_mbmc(pg_base, type, type, MODE_S, vaddr);
+  }
+#endif
+#ifdef CONFIG_RVH
+  if (!check_permission(&pte, true, vaddr, type, virt, mode)) return MEM_RET_FAIL;
+#else
+  if (!check_permission(&pte, true, vaddr, type)) return MEM_RET_FAIL;
+#endif
   #ifdef CONFIG_RVH
   if(virt){
     pg_base = gpa_stage(pg_base | (vaddr & PAGE_MASK), vaddr, type, type, hlvx, false) & ~PAGE_MASK;
@@ -1084,7 +1086,7 @@ bool pmptable_check_permission(word_t offset, word_t root_table_base, int type, 
 #endif
 
 #ifdef CONFIG_RV_MBMC
-bool isa_bmc_check_permission(paddr_t addr, int len, int type, int out_mode) {
+bool isa_bmc_check_permission(paddr_t addr, int type, int trap_type, int mode, vaddr_t vaddr) {
   if (mbmc->BME == 0) {
     return true;
   }
@@ -1092,9 +1094,8 @@ bool isa_bmc_check_permission(paddr_t addr, int len, int type, int out_mode) {
     return true;
   }
   word_t bm_base = (mbmc->BMA) << 6;
-  // word_t ppn = ((addr >> PGSHFT));
-  word_t ppn = (addr >> (9 * pt_level + PGSHFT) << (9 * pt_level));
-  bool is_bmc = (bitmap_read(bm_base + ppn / 8, MEM_TYPE_BM_READ, out_mode) >> (ppn % 8)) & 1;
+  word_t ppn = ((addr >> PGSHFT));
+  bool is_bmc = (bitmap_read(bm_base + ppn / 8, MEM_TYPE_BM_READ, trap_type, mode, vaddr) >> (ppn % 8)) & 1;
   return !is_bmc;
 }
 #endif
